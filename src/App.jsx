9786328199eb4618da26412220b4e316fa507293
Ayyappa818 <ayyappa666swamy@gmail.com>
2024-10-22 11:59:52 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import './App.css';
 import Counter from './features/counter/Counter';
 import products from './features/products/products.json'
 import ProductList from './features/products/ProductList';
+import { useSelector } from 'react-redux';
 function App() {
+  var {cartItems}= useSelector(state=>state.productReducer)
   return (
     <div>
       <Counter></Counter>
+      <h2>CartItemsCount: {cartItems?.length}</h2>
       <ProductList></ProductList>
     </div>
   );
--- src/features/products/productSlice.js
+++ src/features/products/productSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   products,
-  CartItems:[]
+  cartItems:[]
 }
 
 export const productSlice = createSlice({
@@ -11,7 +11,7 @@ export const productSlice = createSlice({
   initialState,
   reducers: {
    addToCart:(state,action)=>{
-    state.CartItems.push(action.payload)                        
+    state.cartItems.push(action.payload)                        
    }
   },
 })
