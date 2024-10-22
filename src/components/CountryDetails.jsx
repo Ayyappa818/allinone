cdb1c026487a90b154d1c1b6abae4aa4f407aa38
Ayyappa818 <ayyappa666swamy@gmail.com>
2024-10-22 19:16:11 +0530

    changes

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -3,9 +3,9 @@ import { useParams,useLocation } from 'react-router-dom';
 
 function CountryDetails() {
   var [d,setD]=React.useState({})
-
+  // console.log(d.population)
     var x = useParams()
-    // console.log(x)
+    console.log(x)
    
     useEffect(function(){
       fetch(`https://restcountries.com/v3/name/${x.cname}`).then(function(res){
