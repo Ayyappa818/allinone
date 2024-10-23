a618ae30bcc8194e8df1bb436b199aa38151b62d
Ayyappa818 <ayyappa666swamy@gmail.com>
2024-10-23 19:45:55 +0530

    changes

--- src/app/store.js
+++ src/app/store.js
@@ -3,14 +3,16 @@ import CounterReducer from '../features/counter/CounterSlice'
 import productReducer from '../features/products/productSlice'
 import { productsApi } from '../services/Product'
 import { setupListeners } from '@reduxjs/toolkit/query'
+import { todosApi } from '../services/Todos'
 export const store = configureStore({
   reducer: {
     counter:CounterReducer,
     productReducer,
     [productsApi.reducerPath]: productsApi.reducer,
+    [todosApi.reducerPath]:todosApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(productsApi.middleware),
+    getDefaultMiddleware().concat(productsApi.middleware,todosApi.middleware),
 })
 setupListeners(store.dispatch)
 
--- src/features/todolist/Todolist.jsx
+++ src/features/todolist/Todolist.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useAddTodoMutation } from '../../services/Todos'
 
 function Todolist() {
+  var [addTodoFn]=useAddTodoMutation()
   return (
     <div>
-      <input type="text" name="" id="" />
-      <button>Add Todo</button>
+      <input type="text" name="" id="d1" />
+      <button onClick={()=>{addTodoFn({title:document.getElementById("d1").value})}}>Add Todo</button>
     </div>
   )
 }
